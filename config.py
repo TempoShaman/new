bfb27f48d5ab665fc068718d9e7d14b5aa2624c9
TempoShaman <80389825+TempoShaman@users.noreply.github.com>
2022-06-21 12:04:09 +0200

    work

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+DATA_PATH = (r"C:\Users\olive\Documents\few-shot\datasets")
+
+EPSILON = 1e-8
+
+if DATA_PATH is None:
+    raise Exception('Configure your data folder location in config.py before continuing!')
